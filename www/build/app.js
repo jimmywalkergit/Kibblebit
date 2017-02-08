ad8435329fad9651aecc812fef7cd76c74972425
James <jimmy.the.walk@gmail.com>
2017-02-08 22:36:59 +0000

    working http php request to external server

--- www/build/app.js
+++ www/build/app.js
@@ -132,7 +132,7 @@ angular.module('App', ['ionic', 'ngCordova', 'ngAnimate'])
 
 })(angular, ionic);
 window.queries = [
-	//Drop tables
+/*	//Drop tables
    "DROP TABLE IF EXISTS Users;",
 	//Create tables
 	"CREATE TABLE Users (IdUser integer primary key autoincrement, Name text not null);",
@@ -147,6 +147,7 @@ window.queries = [
 	"INSERT INTO 'Users' ('Name') VALUES ('Luis Eduardo Oquendo Pérez');",
 	"INSERT INTO 'Users' ('Name') VALUES ('Carlos Rojas');",
 	"INSERT INTO 'Users' ('Name') VALUES ('Levano Castilla Carlos Miguel');"
+	*/
 ];
 (function () {
 	'use strict';
@@ -354,8 +355,8 @@ window.queries = [
         .module('App')
         .controller('AppController', AppController);
 
-    AppController.$inject = ['$scope', '$ionicPopover', '$ionicPopup'];
-    function AppController($scope, $ionicPopover, $ionicPopup) {
+    AppController.$inject = ['$scope', '$ionicPopover', '$ionicPopup', '$http'];
+    function AppController($scope, $ionicPopover, $ionicPopup, $http) {
         
         $scope.items = [
             {
@@ -412,6 +413,24 @@ window.queries = [
 			});
         };
         
+            $scope.data = {};
+ 
+    $scope.submit = function(){
+        var link = 'http://nikola-breznjak.com/_testings/ionicPHP/api.php';
+ 
+        $http.post(link, {username : $scope.data.username}).then(function (res){
+        	
+           $scope.response = res.data;
+           
+           $ionicPopup.alert({
+				title: 'Hi this is your pet :D',
+				template: $scope.response,
+     		cssClass: 'animated bounceInDown'
+			});
+           
+           
+        });
+    };
 
         $scope.$on('$destroy', function() {
             $scope.popover.remove();
